f5c0fb4d831055cc6a6488fa36a1432b14d88b52
Kiyamou <56406588+Kiyamou@users.noreply.github.com>
2022-07-13 21:57:58 +0800

    Update error information for tilesize

--- src/main.cpp
+++ src/main.cpp
@@ -200,13 +200,13 @@ static void VS_CC filterCreate(const VSMap* in, VSMap* out, void* userData, VSCo
         if (err)
             tilesize_x = 0;
         if (tilesize_x != 0 && tilesize_x < 32)
-            throw std::string{ "block_w must be >= 32 or set as 0" };
+            throw std::string{ "tilesize_x must be >= 32 or set as 0" };
 
         int tilesize_y = int64ToIntS(vsapi->propGetInt(in, "tilesize_y", 0, &err));
         if (err)
             tilesize_y = tilesize_x;
         if (tilesize_y != 0 && tilesize_y < 32)
-            throw std::string{ "block_h must be >= 32 or set as 0" };
+            throw std::string{ "tilesize_y must be >= 32 or set as 0" };
 
         // More fine-grained tilesize policy here
         uint32_t heap_budget = ncnn::get_gpu_device(gpuId)->get_heap_budget();
